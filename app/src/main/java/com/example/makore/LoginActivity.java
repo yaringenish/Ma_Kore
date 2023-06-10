b323cb41e54c3c74910cbdeafbd1a710aefb2169
Yarin Genish <yaringenish@gmail.com>
2023-06-11 01:07:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/makore/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.makore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.makore.databinding.ActivityLoginBinding;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
+    private String username;
+    private String password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleLogin();
+        handleRegister();
+    }
+
+    private void handleLogin() {
+        binding.btnLogin.setOnClickListener(view -> {
+            username = binding.etLoginUsername.getText().toString();
+            password = binding.etLoginPassword.getText().toString();
+            //validate
+            //post request for token
+            //setUsername
+            //if valid username and password go to chat activity and transfer the token and username to the new intent
+            finish();
+        });
+    }
+    private void handleRegister(){
+        binding.btnLoginRegister.setOnClickListener(view -> {
+            //start registerActivity
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            //no need for finish because when finishing register will return to here.
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/makore/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.makore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String token;
+    private String username;
+    private int view = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
