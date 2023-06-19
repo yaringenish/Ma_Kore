942b6ea433fad375d933c6dd6ba18a2c4d265c82
Yarin Genish <yaringenish@gmail.com>
2023-06-19 23:27:20 +0300

    Updated

--- app/src/main/java/com/example/makore/AppDB.java
+++ app/src/main/java/com/example/makore/AppDB.java
@@ -2,6 +2,9 @@ package com.example.makore;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import com.example.makore.dao.ChatDao;
 import com.example.makore.dao.ChatItemDao;
@@ -9,8 +12,11 @@ import com.example.makore.entities.Chat;
 import com.example.makore.entities.ChatListItem;
 import com.example.makore.entities.Message;
 import com.example.makore.entities.User;
+import com.example.makore.typeconverters.ChatItemListTypeConverter;
+import com.example.makore.typeconverters.ChatTypeConverter;
 
 @Database(entities = {ChatListItem.class,Chat.class, User.class, Message.class} , version = 1)
+@TypeConverters({ChatTypeConverter.class, ChatItemListTypeConverter.class})
 public abstract class AppDB extends RoomDatabase {
     public abstract ChatItemDao chatItemDao();
 
